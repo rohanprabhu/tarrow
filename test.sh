a44cc270820d1012e9e90d7ea00f263d39125bb0
Rohan Prabhu <rohan@rohanprabhu.com>
2024-05-19 15:31:07 +0530

    Added setup, run diesel migrations before test and add healthcheck to postgres test container

--- test.sh
+++ test.sh
@@ -1,7 +1,10 @@
 set -e
 
 docker compose -f test-setup/docker-compose.yaml down
-docker compose -f test-setup/docker-compose.yaml up -d
+docker compose -f test-setup/docker-compose.yaml up --wait
+
+.local-root/bin/diesel migration run --database-url 'postgres://tarrow-dev:tarrow-dev@localhost:35431/tarrow-test-db'
+
 if [ "$1" == "watch" ]; then
   ec=`cargo watch -x "test -- --nocapture"`
 else
